8e4f0fc87d1e6a1acbb49db7c30c6dbfba859197
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-12 16:52:12 +0300

    Restyled welcome section

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -4,11 +4,17 @@ export const WelcomeSection = () => {
   return (
     <div className='welcome-section'>
         <h3>Hello there,<br></br> I'm Mihail Shterev</h3>
-        <div className="orbit">
-        <div className="star"></div>
-            <div className="orbiting-ellipse"></div>
+        <div className="welcome-section-curved-line"/>
+        <div className="welcome-section-curved-line-left-up"/>
+        <div className="welcome-section-horizonal-line"/>
+        <div className='galaxy-container'>
+          <div className="orbit">
+              <div className="star"></div>
+          </div>
+          <div className="orbit-2"></div>
+          <div className="orbit-3"></div>
+          <div className="orbit-4"></div>
         </div>
-
     </div>
   )
 }
