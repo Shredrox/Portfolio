4f2dc1211613e3921a3751b47623658f6d6e30d1
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-12 19:55:00 +0300

    Added solar system and styling on projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const ProjectsSection = ({elRef}) => {
+  const [projectSelected, setProjectSelected] = useState('');
+
+  function selectProject(id){
+    if(id == projectSelected){
+      id = '';
+    }
+    setProjectSelected(id);
+  }
+
   return (
     <div ref={elRef} className='projects-section'>
       <div className='straight-line-short'></div>
@@ -9,6 +19,22 @@ export const ProjectsSection = ({elRef}) => {
       <div className='straight-line-long'></div>
       <div className='about-me-text'>
         <h2> <div className="point"/>Projects</h2>
+        <div className='solar-system'>
+          <div className='ellipse'></div>
+          <div className="solar-orbit1">
+            <div onClick={() => selectProject('1')} className={projectSelected == '1' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+          </div>
+          <div className="solar-orbit2">
+            <div onClick={() => selectProject('2')} className={projectSelected == '2' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+          </div>
+          <div className="solar-orbit3">
+            <div onClick={() => selectProject('3')} className={projectSelected == '3' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="project-container">
+        <h3>Click on a planet to view a project</h3>
       </div>
     </div>
   )
