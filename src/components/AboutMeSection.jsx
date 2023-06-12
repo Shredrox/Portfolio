a81792477e597d4b91b51f83ddd0a34db7f3ad68
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-12 16:52:39 +0300

    Restyled about me section

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
+import { motion, transform } from 'framer-motion'
+import htmlImg from '../assets/html.png'
+import cssImg from '../assets/css.png'
+import jsImg from '../assets/js.png'
+import reactImg from '../assets/react.png'
+import csharpImg from '../assets/csharp.png'
+import dotnetImg from '../assets/dotnet.jpg'
 
 export const AboutMeSection = ({refe}) => {
+  
   return (
     <div ref={refe} className='about-me'>
-      <div className='about-me-text'>
-        <h2>About Me</h2>
+      <div className='straight-line-short'></div>
+      <div className='curved-line-down'></div>
+      <div className='curved-line-up'></div>
+      <div className='straight-line-long'></div>
+      <div 
+        className='about-me-text'>
+        <h2> <div className="point"/>About Me</h2>
         <p>
           Hi, my name's Mihail Shterev. I'm a Computer Science student at the Technical University of Sofia, Bulgaria.
           I have a passion for computers, how they work and how i can use them in interesting ways to do cool stuff.
           I'm eager to learn and to always improve my programming skills. I'm currently learning web development.
         </p>
+      </div>
+
+      <div className='about-me-stack'>
+        <h2>Tech Stack</h2>
+        <h4>Front End</h4>
+        <div className='tech-stack'>
+          <img src={htmlImg} alt="" />
+          <img src={cssImg} alt="" />
+          <img src={jsImg} alt="" />
+          <img src={reactImg} alt="" />
+        </div>
+        <h4>Back End</h4>
+        <div className='tech-stack'>
+          <img src={csharpImg} alt="" />
+          <img src={dotnetImg} alt="" />
+        </div>
       </div>
     </div>
   )
