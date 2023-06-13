552d8bba79925f45dbf87143dc01b4318bb492cf
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-13 09:18:08 +0300

    Added links

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import linkedinIcon from '../assets/linkedinIcon.png'
+import githubIcon from '../assets/githubIcon.png'
+import spotifyIcon from '../assets/spotifyIcon.png'
+import instagramIcon from '../assets/instagramIcon.webp'
 
 export const ContactSection = ({elRef}) => {
   return (
     <div ref={elRef} className='contact-section'>
         <div className='straight-line-short'></div>
         <div className='curved-line-down'></div>
-        <div className='curved-line-up'></div>
-        <div className='straight-line-long'></div>
-        <div className="meteor">
-            
-        </div>
+        <div className="contact-section-horizonal-line"></div>
 
         <div className="contact-form">
             <h2>Contact Me</h2>
@@ -18,6 +18,20 @@ export const ContactSection = ({elRef}) => {
             <textarea type="text" className='contact-input-msg' placeholder='Message' />
             <button className='glow-on-hover' >Send</button>
         </div>
+        <h3>My Links: 
+          <a href="https://www.linkedin.com/in/mihail-shterev-92b149254/" target='_blank'>
+            <img  src={linkedinIcon} alt="" />
+          </a>
+          <a href="https://github.com/Shredrox" target='_blank'>
+            <img src={githubIcon} alt="" />
+          </a>
+          <a href="https://open.spotify.com/artist/5x0JssU826Vd1fIz7scsPu?si=D9hg61oVS-WuVgWIxm5MTQ" target='_blank'>
+            <img src={spotifyIcon} alt="" />
+          </a>
+          <a href="https://www.instagram.com/xthatmikey/" target='_blank'>
+            <img src={instagramIcon} alt="" />
+          </a>
+        </h3>
     </div>
   )
 }
