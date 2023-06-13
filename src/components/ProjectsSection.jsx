d92f1f7d03336dfc13366054e77e853903c9637c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-13 10:48:04 +0300

    Added projects to projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
+import solarSystemSim from '../assets/projects/ssSimulation.jpg'
+import logicalExpressionInt from '../assets/projects/logicalexpressioninterpreter.png'
+import musicUniverse from '../assets/projects/musicUniverse.png'
+import musicPlayer from '../assets/projects/musicPlayer.jpg'
 
 export const ProjectsSection = ({elRef}) => {
   const [projectSelected, setProjectSelected] = useState('');
+  const [image, setImage] = useState(null);
+  const [projectTitle, setProjectTitle] = useState('Click on a planet to view a project');
+  const [projectLink, setProjectLink] = useState('');
 
-  function selectProject(id){
+  function selectProject(id, image, projectTitle, projectLink){
     if(id == projectSelected){
       id = '';
+      image = null;
+      projectTitle = 'Click on a planet to view a project';
+      projectLink = '';
     }
+    
     setProjectSelected(id);
+    setImage(image);
+    setProjectTitle(projectTitle);
+    setProjectLink(projectLink);
   }
 
   return (
@@ -16,25 +30,41 @@ export const ProjectsSection = ({elRef}) => {
       <div className='straight-line-short'></div>
       <div className='curved-line-down'></div>
       <div className='curved-line-up'></div>
-      <div className='straight-line-long'></div>
+      <div className='projects-straight-line-long'></div>
+
       <div className='about-me-text'>
         <h2> <div className="point"/>Projects</h2>
         <div className='solar-system'>
           <div className='ellipse'></div>
           <div className="solar-orbit1">
-            <div onClick={() => selectProject('1')} className={projectSelected == '1' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+            <div 
+            onClick={() => selectProject('1', solarSystemSim, '2D Planetary System Simulation', 'https://github.com/Shredrox/2D-Planetary-System-Simulation')} 
+            className={projectSelected == '1' ? 'selected-planet' : 'orbiting-ellipse'}></div>
           </div>
           <div className="solar-orbit2">
-            <div onClick={() => selectProject('2')} className={projectSelected == '2' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+            <div 
+            onClick={() => selectProject('2', logicalExpressionInt, 'Logical Expression Interpreter', 'https://github.com/Shredrox/LogicalExpressionInterpreter')} 
+            className={projectSelected == '2' ? 'selected-planet' : 'orbiting-ellipse'}></div>
           </div>
           <div className="solar-orbit3">
-            <div onClick={() => selectProject('3')} className={projectSelected == '3' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+            <div 
+            onClick={() => selectProject('3', musicUniverse, 'Music Universe' , 'https://github.com/Shredrox/Music-Universe')} 
+            className={projectSelected == '3' ? 'selected-planet' : 'orbiting-ellipse'}></div>
+          </div>
+          <div className="solar-orbit4">
+            <div 
+            onClick={() => selectProject('4', musicPlayer, 'Music Player' , 'https://github.com/Shredrox/Music-Player')} 
+            className={projectSelected == '4' ? 'selected-planet' : 'orbiting-ellipse'}></div>
           </div>
         </div>
       </div>
       
       <div className="project-container">
-        <h3>Click on a planet to view a project</h3>
+        <h3>{projectTitle}</h3>
+        <img src={image} alt="" />
+        {projectLink !== '' &&
+        <a href={projectLink} target='_blank'><button>Github Repo</button></a>
+        }
       </div>
     </div>
   )
