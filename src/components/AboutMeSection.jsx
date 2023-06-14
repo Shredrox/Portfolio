fa12c420313ed46b393ff610cc3d9dbdf3d51972
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-14 14:52:49 +0300

    Changed elements style and positioning

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -11,35 +11,40 @@ export const AboutMeSection = ({elRef}) => {
   
   return (
     <div ref={elRef} className='about-me'>
-      <div className='straight-line-short'></div>
-      <div className='curved-line-down'></div>
-      <div className='curved-line-up'></div>
-      <div className='straight-line-long'></div>
-      <div 
-        className='about-me-text'>
-        <h2> <div className="point"/>About Me</h2>
-        <p>
-          Hi, my name's Mihail Shterev. I'm a Computer Science student at the Technical University of Sofia, Bulgaria.
-          I have a passion for computers, how they work and how i can use them in interesting ways to do cool stuff.
-          I'm eager to learn and to always improve my programming skills. I'm currently learning web development.
-        </p>
-      </div>
+      <div className="about-me-container">
+        <div className='lines-container'>
+           <div className='straight-line-short'></div>
+           <div className="point"/>
+           <div className='straight-line-long'></div>
+        </div>
+        
+        <div 
+          className='about-me-text'>
+          <h2> About Me</h2>
+          <p>
+            Hi, my name's Mihail Shterev. I'm a Computer Science student at the Technical University of Sofia, Bulgaria.
+            I have a passion for computers, how they work and how i can use them in interesting ways to do cool stuff.
+            I'm eager to learn and to always improve my programming skills. I'm currently learning web development.
+          </p>
+        </div>
 
-      <div className='about-me-stack'>
-        <h2>Tech Stack</h2>
-        <h4>Front End</h4>
-        <div className='tech-stack'>
-          <img src={htmlImg} alt="" />
-          <img src={cssImg} alt="" />
-          <img src={jsImg} alt="" />
-          <img src={reactImg} alt="" />
-        </div>
-        <h4>Back End</h4>
-        <div className='tech-stack'>
-          <img src={csharpImg} alt="" />
-          <img src={dotnetImg} alt="" />
+        <div className='about-me-stack'>
+          <h2>Tech Stack</h2>
+          <h4>Front End</h4>
+          <div className='tech-stack'>
+            <img src={htmlImg} alt="" />
+            <img src={cssImg} alt="" />
+            <img src={jsImg} alt="" />
+            <img src={reactImg} alt="" />
+          </div>
+          <h4>Back End</h4>
+          <div className='tech-stack'>
+            <img src={csharpImg} alt="" />
+            <img src={dotnetImg} alt="" />
+          </div>
         </div>
       </div>
+      
     </div>
   )
 }
--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -3,17 +3,23 @@ import React from 'react'
 export const WelcomeSection = ({elRef}) => {
   return (
     <div ref={elRef} className='welcome-section'>
-        <h3>Hello there,<br></br> I'm Mihail Shterev</h3>
-        <div className="welcome-section-curved-line"/>
-        <div className="welcome-section-curved-line-left-up"/>
-        <div className="welcome-section-horizonal-line"/>
-        <div className='galaxy-container'>
-          <div className="orbit">
-              <div className="star"></div>
+        <div className="welcome-section-container">
+          <h3>Hello there,<br></br> I'm Mihail Shterev</h3>
+          <div className='galaxy-container'>
+            <div className="orbit">
+                <div className="star"></div>
+            </div>
+            <div className="orbit-2"></div>
+            <div className="orbit-3"></div>
+            <div className="orbit-4"></div>
           </div>
-          <div className="orbit-2"></div>
-          <div className="orbit-3"></div>
-          <div className="orbit-4"></div>
+        </div>
+        
+        <div className="welcome-section-lines-container">
+          <div className="point2"></div>
+          <div className="welcome-section-curved-line"/>
+          <div className="welcome-section-horizonal-line"/>
+          <div className="welcome-section-curved-line-left-up"/>
         </div>
     </div>
   )
