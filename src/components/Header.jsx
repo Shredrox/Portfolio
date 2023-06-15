505d677433064480007ffaddd00788ab9694e3a4
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-15 19:38:42 +0300

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import mIcon from '../assets/favicon.ico'
 
 export const Header = ({scroll, homeRef, aboutMeRef, projectsRef, contactsRef}) => {
   return (
     <div>
-      <ul className='header'>
-        <li><button onClick={() => scroll(homeRef)}>Home</button></li>
-        <li><button onClick={() => scroll(aboutMeRef)}>About Me</button></li>
-        <li><button onClick={() => scroll(projectsRef)}>Projects</button></li>
-        <li><button onClick={() => scroll(contactsRef)}>Contact</button></li>
-      </ul>
+      <div className='header'>
+        <img src={mIcon} alt="" />
+        <ul>
+          <li><button onClick={() => scroll(homeRef)}>Home</button></li>
+          <li><button onClick={() => scroll(aboutMeRef)}>About Me</button></li>
+          <li><button onClick={() => scroll(projectsRef)}>Projects</button></li>
+          <li><button onClick={() => scroll(contactsRef)}>Contact</button></li>
+        </ul>
+      </div>
     </div>
   )
 }
