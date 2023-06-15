a787cd67c3c74ac3a245cf359c1f8319f2cb170e
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-15 22:34:52 +0300

    Added hamburger menu for nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import mIcon from '../assets/favicon.ico'
+import { FaBars, FaDribbble }  from 'react-icons/fa'
+import { useState, useEffect } from 'react';
+import { NavBar } from './NavBar';
 
 export const Header = ({scroll, homeRef, aboutMeRef, projectsRef, contactsRef}) => {
+  const [burgirVisible, setBurgirVisible] = useState(false);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleBurgir = () => {
+    setBurgirVisible(!burgirVisible);
+  }
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+  }, []);
+
   return (
     <div>
       <div className='header'>
         <img src={mIcon} alt="" />
+        {windowWidth > 660 ? 
         <ul>
           <li><button onClick={() => scroll(homeRef)}>Home</button></li>
           <li><button onClick={() => scroll(aboutMeRef)}>About Me</button></li>
           <li><button onClick={() => scroll(projectsRef)}>Projects</button></li>
           <li><button onClick={() => scroll(contactsRef)}>Contact</button></li>
-        </ul>
+        </ul> 
+        :
+        <FaBars className='burgir' onClick={toggleBurgir}/> 
+        }
+        
       </div>
+      {burgirVisible && windowWidth < 660 &&
+        <NavBar scroll={scroll} elRefs={[homeRef, aboutMeRef, projectsRef, contactsRef]}/>
+      }
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavBar = ({scroll, elRefs}) => {
+  return (
+    <div className='nav-bar'>
+        <ul>
+            <li><button onClick={() => scroll(elRefs[0])}>Home</button></li>
+            <li><button onClick={() => scroll(elRefs[1])}>About Me</button></li>
+            <li><button onClick={() => scroll(elRefs[2])}>Projects</button></li>
+            <li><button onClick={() => scroll(elRefs[3])}>Contact</button></li>
+        </ul>
+    </div>
+  )
+}
